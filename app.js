e6b0bb3b024debeaef96b503c0fa56245fd13fe6
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-03-22 17:43:28 +0100

    EDIT printo on html todo list

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            Title: 'ToDo List',
+    data() {
+        return {
+            title: 'ToDo List',
+            toDo: [],
+            newToDo: '',
         }
-    }
-
+    },
+    methods:{
+        newTask(){
+            this.newToDo
+        },
+        fetchTodoList(){
+            axios
+            .get('./server.php')
+            .then((res) => {
+                console.log(res.data)
+                this.toDo = res.data
+            })
+            .catch((err) => {
+                this.toDo = []
+            })
+        }
+    },
+    mounted(){
+        this.fetchTodoList()
+    },
 }).mount('#app')
